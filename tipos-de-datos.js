c9400f7755dc32323a5462b69fe2968cd86ade7b
Monica <monica.espinosa.hernandez508@gmail.com>
2025-02-04 20:15:38 -0600

    clase 1

---
+++ tipos-de-datos.js
@@ -0,0 +1,23 @@
+//imprimir en consola
+console.log("hola mundo");
+
+//declaracion de variables y asignacion de valores
+var nombre = "Monica"; //string
+var edad = 23; //number
+var sueldo = 12.23; // number
+var casada = true; //boolean
+var hijos = null; // null
+var dato; // undefined
+
+//saber tipo de dato con typeof
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(hijos));
+console.log(typeof(dato));
+
+// la semantica es imprtante en el significado de las palabras
+var pais = 20;
+var nombre = Francia;
+
+// si ponemos typeof de null dira que es object pero en realidd es aucencia intencional de valor por un error en javascript que aun no se ha corregido
