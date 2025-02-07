4b34981c0bb070c952c9b27ba9a4b51143e3ca07
Monica <monica.espinosa.hernandez508@gmail.com>
2025-02-06 20:28:23 -0600

    Ejercicio Segunda Clase 2

---
+++ operadores-basicos.js
@@ -0,0 +1,57 @@
+
+let valor_1 = 13;
+let valor_2 = 2;
+
+//Operaciones Basicas
+
+let suma = valor_1 + valor_2;
+console.log(suma);
+
+let resta = valor_1 - valor_2;
+console.log(resta);
+
+let multiplicacion = valor_1 * valor_2;
+console.log(multiplicacion);
+
+let division = valor_1 / valor_2;
+console.log(division);
+
+let modulo = valor_1 % valor_2;
+console.log(modulo);
+
+let exponencial = valor_1 ** valor_2;
+console.log(exponencial);
+
+// operadores de comparacion
+
+let numero_1 = 9;
+let numero_2 = "9";
+
+console.log(numero_1==numero_2);
+console.log(numero_1===numero_2);
+
+console.log("Operadores logicos y de comparacion");
+
+console.log(10>4);
+console.log(2<1);
+console.log(10>=10);
+console.log(10<=10);
+
+//La respuesta sera en valor booleano
+console.log("Diferente de: ");
+
+console.log(15!=15); //falso porque 15 y 15 es el mismo valor
+
+
+console.log("and: &&");
+console.log("or: ||");
+console.log("not: !");
+
+let calificacion = 10;
+let calificacion1 = 9;
+let calificacion2 = 8;
+let calificacion3 = 7;
+let calificacion4 = 1;
+
+//+= nos permite hacer unasuma acumulatoria
+
---
+++ variables.js
@@ -0,0 +1,19 @@
+// Clase 2
+
+var nombre = "Ana";
+var nombre = "Fernanda";
+var nombre = "Victor";
+
+console.log(nombre);
+
+
+// Se recomienda usar Let en la actualidad
+
+let apellido1 = "Espinosa";
+let apellido2 = "Hernandez";
+
+//Let si permite cambiar el valor
+
+apellido1 = "Garcia";
+
+console.log(apellido1);
